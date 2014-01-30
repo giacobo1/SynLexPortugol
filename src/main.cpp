dbfd4d0f7df3eafe63c49c560f598f6921e891b9
Bruno G. Pinto <bgpinto@inf.ufpel.edu.br>
2014-01-30 14:06:11 -0200

    Locale adicionado.

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,17 @@
 int main(int argc, char const *argv[])
 {
 
+	setlocale(LC_CTYPE, "");
+
+	if(argc == 2)
+	{
+		printf("Ok!");
+	}
+	else
+	{
+		printf("Erro na passagem de arquivos.\n");
+		exit(1);
+	}
+
 	return EXIT_SUCCESS;
 }
